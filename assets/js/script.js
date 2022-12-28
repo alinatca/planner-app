1f8b438b5a9888674c04d74cd88026de1d1d9da3
unknown <alina_tca@yahoo.com>
2022-12-28 10:51:16 +0000

    Creating local storages for each hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,5 +40,19 @@ $(document).ready(function () {
       }
     
       hourUpdater();
+
+      // set up interval to check if current time needs to be updated
+    var interval = setInterval(hourUpdater, 15000);
+  
+    // load any saved data from localStorage
+      $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+      $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+      $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+      $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+      $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+      $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+      $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+      $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+      $("#hour-17 .description").val(localStorage.getItem("hour-17"));
   
 });
